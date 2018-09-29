1111e0fa479607a2db2a0c023879511e312aae06
MAMOPC\Mamokin <m.seaward@pbsa.info>
2018-09-29 14:44:34 -0300

    setup attempt. not working on windows OS

---
+++ genysys-gm/src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'genysys-gm-electron',
+  };
+</script>
+
+<style>
+  /* CSS */
+</style>
